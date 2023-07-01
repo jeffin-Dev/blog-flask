f2c67f4ebc6532e3e6d71fe111c4d0530f6f4055
Jefferson Roberto <135037032+jeffin-Dev@users.noreply.github.com>
2023-07-01 14:39:19 -0300

    Update __init__.py

--- ProjectBlogJeff/__init__.py
+++ ProjectBlogJeff/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-import sqlalchemy
+import sqlalchemy.dialects.postgresql
+
 
 app = Flask(__name__)
 
