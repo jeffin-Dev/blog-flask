fb422f3921684a499f8d611969ae94bd1dfbf966
Jefferson Roberto <135037032+jeffin-Dev@users.noreply.github.com>
2023-07-01 10:21:18 -0300

    final
    final

--- ProjectBlogJeff/__init__.py
+++ ProjectBlogJeff/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+import os
+
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'd24f396f2a891195866947f4df0e1e96'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
