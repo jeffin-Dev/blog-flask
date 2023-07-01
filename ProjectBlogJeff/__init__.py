567788467d6417f3fcb2a3f04980c45d7c7d2166
Jefferson Roberto <135037032+jeffin-Dev@users.noreply.github.com>
2023-07-01 14:33:32 -0300

    Update __init__.py

--- ProjectBlogJeff/__init__.py
+++ ProjectBlogJeff/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
-
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -20,6 +20,17 @@ login_manager = LoginManager(app)
 login_manager.login_view='login'
 login_manager.login_message='Faça Login Ou Cadastre-se Para Acessar'
 login_manager.login_message_category='alert alert-danger'
+
+from ProjectBlogJeff import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print ('data de base criada')
+else:
+    print ('data de base já existente')
 
 
 from ProjectBlogJeff import routes
